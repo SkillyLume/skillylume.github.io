2d9970c4a062c3fcc13945be9c6dcc5c0fdcd529
Radmir <uzyanbaevradmir@gmail.com>
2023-12-29 16:05:01 +0500

    Added: Christmas garland, snowfall effect, minor adjustments

--- src/js/script.js
+++ src/js/script.js
@@ -19,3 +19,11 @@ function checkAnswer(taskNumber, correctAnswer) {
     resultElement.style.color = userAnswer === correctAnswer ? "green" : "red";
     resultElement.textContent = userAnswer === correctAnswer ? "Правильно" : "Неправильно";
 }
+
+function gir() {
+    let nums = document.getElementById('gir').className; 
+    if(nums == 'gir_1') {document.getElementById('gir').className='gir_2';}  
+    if(nums == 'gir_2') {document.getElementById('gir').className='gir_3';}  
+    if(nums == 'gir_3') {document.getElementById('gir').className='gir_1';} 
+}  
+setInterval(gir, 500);
